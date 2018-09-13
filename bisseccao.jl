7b7a248c3c49758fbdf3bdc6fc515ac45c55986c
Janaina Cristina <janainacm@msn.com>
2018-09-13 16:58:45 -0300

    passamos o codigo para o git

--- bisseccao.jl
+++ bisseccao.jl
@@ -1,27 +1,54 @@
-function bisseccao(f, a, b, estrategia; atol=1e-6, rtol=1e-6, max_iter=10_000)
-    fa, fb = f(a), f(b)
-    if abs(fa) < atol
+function bisseccao(f ::Function, g ::Function, a::Real, b::Real; atol=1e-6, rtol=1e-6, maxiter=10_000)
+    (fa, fb) = (f(a), f(b))
+    ϵ = atol + rtol * (abs(fa) + abs(fb)) / 2
+    if abs(fa) <= ϵ
         return a, fa, 0
-    elseif abs(fb) < atol
+    elseif abs(fb) <= ϵ
         return b, fb, 0
+    elseif fa * fb > 0
+        error("f(a) e f(b) devem ter sinais diferentes")
     end
-    if fa * fb > 0
-        error("Hipóteses não satisfeitas")
-    end
-    ϵ = atol + rtol * (fa + fb) / 2
-    x = (a + b) / 2
+    x=g(a,b)
+    #x = (a + b) / 2
     fx = f(x)
     iter = 1
-    while !(abs(fx) < ϵ || iter > max_iter)
-        if fa * fx < 0
-            b, fb = x, fx
+    while !(abs(fx) <= ϵ || iter > maxiter) #!negação
+        if fx * fa < 0
+            b = x
         else
-            a, fa = x, fx
+            a = x
         end
-        x = (a + b) / 2
-        fx = f(x)
 
+        x=g(a,b)
+        fx = f(x)
         iter += 1
     end
+
+
     return x, fx, iter
+end
+
+function bisseccao(f, a::Real, b, estrategia)
+
+    g(a,b)=0
+    if estrategia == :bisseccao
+        g(a,b) =0.5*a+0.5*b
+
+    elseif estrategia == :esquerda
+        g(a,b) = 0.1*a+0.9*b
+
+    elseif estrategia == :direita
+        g(a,b) =0.9*a+0.1*b
+
+    elseif estrategia == :aletorio
+        r=rand()
+        g(a,b)=a * r + b * (1 - r)
+
+    elseif estrategia == :falsa_posicao
+        g(a,b)=(a * f(b) - b * f(a)) / (f(b) - f(a))
+
+    end
+
+
+    return bisseccao(f, g, a, b)
 end
--- tests.jl
+++ tests.jl
@@ -6,10 +6,15 @@ include("bisseccao.jl")
 function tests()
     funcoes = [ (x -> 2x - 3, 0, 5),
                 (x -> x^2 - 2, 1, 2),
-                (x -> x * exp(x) - 1, 0, 1) ]
+                (x -> x * exp(x) - 1, 0, 2) ]
     for i = 1:length(funcoes)
         (f, a, b) = funcoes[i]
         # Seu código aqui
+        x,fx,k=bisseccao(f, a, b, :bisseccao)
+        println("exemplo $i")
+        println("x=$x")
+        println("fx=$fx")
+        println("k=$k")
     end
 end
 
